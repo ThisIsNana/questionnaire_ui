3098bc9b704c17415e9c8da7d4fa45b9c40a51ff
ThisIsNana <pw10279@gmail.com>
2023-06-23 17:41:51 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import './stylesheet/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+// import '@fortawesome/fontawesome-free/css/all.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab);
+library.add(fas);
+
+import App from './App.vue'
+import router from './router'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+const app = createApp(App)
+const pinia = createPinia()
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+app.use(createPinia())
+
+app.config.globalProperties.$axios = axios
+
+app.use(pinia)
+app.use(router)
+app.use(VueSweetalert2);
+app.mount('#app')
